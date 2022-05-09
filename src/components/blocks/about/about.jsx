ca3c3f0eb95ee13089ec8ac71969a170981f1a9c
Nikolay Komarov <kny.komarov@gmail.com>
2022-05-09 23:17:08 +0300

    сделает рефактор блока about (стилизация через styled)

--- src/components/blocks/about/about.jsx
+++ src/components/blocks/about/about.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { Title } from "../../ui/title/title";
 import { TITLE_SIZE } from "../../ui/title/consts";
-import "./styles.css";
+import { Address, StyledSection, Text, TextWrapper, WorkTime } from "./styled";
 
 export const About = () => {
   return (
-    <section className="about">
-      <div className="about__wrapper">
+    <StyledSection>
+      <TextWrapper>
         <Title size={TITLE_SIZE.BIG}>Первое в России котокафе</Title>
-        <p className="about__text">
+        <Text>
           Крупнейшее котокафе России, в котором живёт 50 кошек и котов, и каждый
           из них ищет новый дом. Животных можно гладить, фотографировать, играть
           с ними.
-        </p>
-        <p className="about__work-time">Рабочее время с 8:00 до 23:00</p>
-        <p className="about__address">
+        </Text>
+        <WorkTime>Рабочее время с 8:00 до 23:00</WorkTime>
+        <Address>
           Санкт-Петербург, набережная реки Карповки, дом 5, литера П
-        </p>
-      </div>
-    </section>
+        </Address>
+      </TextWrapper>
+    </StyledSection>
   );
 };
